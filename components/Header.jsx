8f8a2f61c9068e8d9dcd40eeed1fc635248b490a
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-14 14:03:37 +0100

    feat: ass logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Style from '../styles/Header.module.scss'
+import Link from 'next/link'
 
 function Header() {
 	return (
-		<div>Header</div>
+		<div className={Style.banniere}>
+			<Link href="/"><a>sims<span>zilu.</span></a></Link>
+		</div>
 	)
 }
 
