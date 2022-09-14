191637292d8d13b9fb975ee303e5c7f799ae7dc5
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-14 16:51:10 +0100

    feat: create flex to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import Style from '../styles/Header.module.scss'
+import styles from '../styles/Header.module.scss'
 import Link from 'next/link'
 
 function Header() {
 	return (
-		<div className={Style.banniere}>
-			<Link href="/"><a>sims<span>zilu.</span></a></Link>
-		</div>
+		<nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/"><a>sims<span>zilu.</span></a></Link>
+      </div>
+      <ul>
+      	<li>
+          <Link href="/" className={styles.link}>Accueil</Link>
+        </li>
+        <li>
+          <Link href="/" >Infos</Link>
+				</li>
+				<li>
+					<Link href="/projets">Services</Link> 
+				</li>
+				<li>
+					<Link href="/services">Contact</Link>  
+				</li>
+      	<li>     
+					<Link href="/nosprogrammes">Agenda</Link>         
+				</li>
+      </ul>
+    </nav>
 	)
 }
 
