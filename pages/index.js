64daac2da48dc3ae3decbd9b7b417fb6b5d757d4
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-16 11:41:46 +0100

    feat: fix

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.scss'
+import Main from './Main'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <title>SimsZilu | Developper Fullstack</title>
         <link rel='icon' href='/favicon.ico'/>
       </Head>
+      <Main />
     </div>
   )
 }
