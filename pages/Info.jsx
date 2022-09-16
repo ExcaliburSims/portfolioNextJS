97e7b07a142c270624efbf14796c4b2ad4d60c1f
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-16 15:24:19 +0100

    feat: add content infos component

--- pages/Info.jsx
+++ pages/Info.jsx
@@ -1,8 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from '../styles/Info.module.scss'
 
 function Info() {
 	return (
-		<div>Info</div>
+		<div className={styles.container}>
+      <p>Infos</p>
+			<h2>Qui suis-je ?</h2>
+			<div>
+        {/* <Image src={AboutImg} className='rounded-xl' alt='/' /> */}
+			</div>
+      <div>    
+				<p>
+					have spent the last 12 years in the fire service working as a
+          professional firefighter/paramedic. I have always had a knack for
+          technology and working with computers. In 2019 I started working
+          with HTML and CSS to make some minor edits on a small business
+          website that I was operating. What I thought was just a few small
+          edits turned into a love for programming.
+				</p>
+			</div>
+    </div>
 	)
 }
 
