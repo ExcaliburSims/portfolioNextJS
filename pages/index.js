ebc8ab3f9498efea7e94d528c8fd47ae8ce39d28
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-16 04:24:01 +0100

    fix: change name of link

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Portfolio|SimsZilu</title>
+        <title>SimsZilu | Developper Fullstack</title>
         <link rel='icon' href='/favicon.ico'/>
       </Head>
       <Header />
