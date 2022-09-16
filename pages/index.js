0a83ede7bfbc39aa1b593177e4c9b17b37f6727b
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-16 04:14:37 +0100

    feat: add Head

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import styles from '../styles/Home.module.scss'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Portfolio|SimsZilu</title>
+        <link rel='icon' href='/favicon.ico'/>
+      </Head>
       <Header />
     </div>
   )
