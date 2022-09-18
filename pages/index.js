1823a7d9df775020d4e79a819ae6154de887438d
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-18 08:20:42 +0100

    feat:add service component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import styles from '../styles/Home.module.scss'
 import Info from './Info'
 import Main from './Main'
+import Service from './Service'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
       <Main />
       <Info />
+      <Service />
     </div>
   )
 }
