2c645a0c0d316ccba0e8eaca993d2f95d58b12a7
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-18 08:17:39 +0100

    feat: create service component

---
+++ pages/Service.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/Service.module.scss";
+
+function Service() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h2 data-entete="Services">Qu'est-ce que je fais ?</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
