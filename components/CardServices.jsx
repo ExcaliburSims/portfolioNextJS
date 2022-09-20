6b3be5ab42d146cae460c2740be38d8234d16a62
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-20 14:33:52 +0100

    feat:make style

--- components/CardServices.jsx
+++ components/CardServices.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "../styles/CardServices.module.scss";
 import { FaMobileAlt } from "react-icons/fa";
 
-function CardServices() {
+function CardServices(props) {
   return (
     <div className={styles.service}>
       <div className={styles.icon}>
-        <FaMobileAlt className={styles.iconpic} />
+        {/* <FaMobileAlt className={styles.iconpic} /> */}
+        {props.icon}
       </div>
-      <h3>Responsive Design</h3>
+      <h3>{props.title}</h3>
       <p>
         remaining essentially unchanged. It was popularised in the 1960s with
         the release of Letraset sheets containing Lorem Ipsum passages, and more
