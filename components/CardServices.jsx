1ea847432bce71dcb31bf6cf9eb1d3967ff6d3c7
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-20 12:32:13 +0100

    fix max-width

--- components/CardServices.jsx
+++ components/CardServices.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "../styles/CardServices.module.scss";
+import { FaMobileAlt } from "react-icons/fa";
 
 function CardServices() {
-  return <div>CardServices</div>;
+  return (
+    <div className={styles.service}>
+      {/* <div className={styles.service__interieur}> */}
+      <div className={styles.icon}>
+        {/* <i class="fas fa-mobile-alt"></i>*/}
+        <FaMobileAlt className={styles.iconpic} />
+      </div>
+      <h3>Responsive Design</h3>
+      <p>
+        remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more
+        recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+      {/* </div> */}
+    </div>
+  );
 }
 
 export default CardServices;
