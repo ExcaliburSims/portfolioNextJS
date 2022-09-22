8fb8302b9538d9ce7d3cbcbc2ace8cadb4e3c7ac
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-22 06:51:02 +0100

    feat:create contact component

---
+++ pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../styles/Contact.module.scss";
+
+function Contact() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h2 data-entete="Contact">Prendre contact</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
