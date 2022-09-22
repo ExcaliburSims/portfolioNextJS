4d12cfc9a82054b752e67e5552ffeef059f46d3f
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-22 14:30:50 +0100

    feat:create footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Footer.module.scss";
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <h3>&#64;2022 Design&#44; Intégration et développement par Siméon</h3>
+    </div>
+  );
+}
+
+export default Footer;
