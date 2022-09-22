ba770d193f9723b9e4be880c60b5d234e27c98f8
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-22 06:52:52 +0100

    fix hover

--- components/CardServices.jsx
+++ components/CardServices.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import styles from "../styles/CardServices.module.scss";
-import { FaMobileAlt } from "react-icons/fa";
 
 function CardServices(props) {
   return (
     <div className={styles.cardService}>
-      <div className={styles.cardIcon}>
-        {/* <FaMobileAlt className={styles.iconpic} /> */}
-        {props.icon}
-      </div>
+      <div className={styles.cardIcon}>{props.icon}</div>
       <h3>{props.title}</h3>
       <p>
         remaining essentially unchanged. It was popularised in the 1960s with
