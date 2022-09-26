81b5e8cec419a15415791768784778750a0646ae
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-26 07:52:24 +0100

    fix className

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -5,23 +5,30 @@ import styles from '../styles/Home.module.scss'
 
 function Main() {
 	return (
-		 <div className={styles.container}>
-			<Header />
-			<div className={styles.presentationCard}>
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.presentationCard}>
         <div className={styles.presentationCard_comment}>
-					<div>
-						<p>Construisons quelque chose ensemble</p>
-            <h1>Je suis <span>Siméon Zilu</span></h1>
-          	<h1>Ingenieur Logiciel</h1>
-						<p>
-							Je me concentre sur la construction d'applications web et mobile front-end et back-end réactives
-          	</p>
-						<Link href="activite"><a>Hire me</a></Link>
+          <div className={styles.bloc_comment}>
+            <p className={styles.paragraphe}>
+              Construisons quelque chose ensemble
+            </p>
+            <h1 className={styles.title}>
+              Je suis <span>Siméon Zilu</span>
+            </h1>
+            <h1 className={styles.title}>Ingenieur Logiciel</h1>
+            <p className={styles.paragraphe}>
+              Je me concentre sur la construction d'applications web et mobile
+              front-end et back-end réactives
+            </p>
+            <Link href="activite">
+              <a>Hire me</a>
+            </Link>
           </div>
         </div>
       </div>
-		</div>
-	)
+    </div>
+  );
 }
 
 export default Main
