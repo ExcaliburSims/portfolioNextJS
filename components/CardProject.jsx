22da38e9e0da85c5d1011d7b14f53d62c4e1dcb0
ExcaliburSims <ximeonzilu@gmail.com>
2022-10-04 20:04:07 +0100

    feat: add props

--- components/CardProject.jsx
+++ components/CardProject.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/CardProject.module.scss";
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 
-function CardProject() {
+function CardProject(props) {
   return (
     <div className={styles.cardProjet}>
       <div className={styles.cardContent}>
-        <img src="/assets/ours.jpg" className={styles.cardImage} alt="projet" />
+        <img src={props.pic.src} className={styles.cardImage} alt="projet" />
         <div className={styles.cardBloc}>
           <Link href="/">
             <a>Voir projet</a>
           </Link>
         </div>
-        <p className="element--projet__titre">Mon google</p>
+        <p className="element--projet__titre">{props.title}</p>
       </div>
     </div>
   );
