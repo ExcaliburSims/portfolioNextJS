83bc24c96f405c23da7f6ef2d231f32562860498
ExcaliburSims <ximeonzilu@gmail.com>
2022-10-10 16:32:24 +0100

    fix

--- pages/Project.jsx
+++ pages/Project.jsx
@@ -7,22 +7,6 @@ import "slick-carousel/slick/slick-theme.css";
 import data from "../data.json";
 
 function Project() {
-  /* const [data, setData] = useState(null);
-  useEffect(() => {
-  }, []); */
-
-  /* const fetchData = () => {
-    fetch(datas)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        //setData(data);
-      });
-  };
-  fetchData(); */
-  //console.log(data);
-  //console.log(data.projects[0].project_title);
-  //console.log(data.services[0].id);
   var settings = {
     dots: true,
     infinite: true,
@@ -32,7 +16,8 @@ function Project() {
     autoplaySpeed: 2000,
     pauseOnHover: true,
   };
-  data.projects.map((dat) => console.log(dat.project_title));
+  /* data.projects.map((dat) => console.log(dat.id));
+  data.projects.map((dayte) => console.log(dayte.project_image)); */
 
   return (
     <div className={styles.container}>
@@ -49,7 +34,6 @@ function Project() {
             pic={datas.project_image}
           />
         ))}
-        {/* <CardProject /> */}
       </Slider>
     </div>
   );
