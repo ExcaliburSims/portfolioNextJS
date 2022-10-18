36f5bff9f4a2a071a9e267f86ded55f1c8276db5
ExcaliburSims <ximeonzilu@gmail.com>
2022-10-18 07:11:16 +0100

    fix

--- pages/Service.jsx
+++ pages/Service.jsx
@@ -23,12 +23,31 @@ function Service() {
         <h2 data-entete="Services">Qu'est-ce que je fais ?</h2>
       </div>
       <div className={styles.content}>
-        <CardServices title="Application Web" icon={laptop} />
-        <CardServices title="Application Mobile" icon={mobile} />
-        <CardServices title="Design Graphique" icon={palette} />
-        <CardServices title="Clean Code" icon={code} />
-        <CardServices title="Seo Marketing" icon={Seo} />
-        <CardServices title="Great Support" icon={mobile} />
+        <CardServices
+          title="Application Web"
+          icon={laptop}
+          description="De la conception au déploiement, je réalise des applications web de divers types."
+        />
+        <CardServices
+          title="Application Mobile"
+          icon={mobile}
+          description="Android, IOS, Windows je suis dans la mise en oeuvre des applications mobiles sur mesure tournant dans plusieurs plateformes."
+        />
+        <CardServices
+          title="Design Graphique"
+          icon={palette}
+          description="UX & UI, je réalise des designs tenant compte des règles et principes pour une bonne expérience utilisateur."
+        />
+        <CardServices
+          title="Clean Code"
+          icon={code}
+          description="J’assure la maintenance des codes déjà existants en les optimisants avec les bonnes pratiques pour une bonne complexité."
+        />
+        <CardServices
+          title="Seo Marketing"
+          icon={Seo}
+          description="J’aide les tiers avec des techniques d’optimisation pour les moteurs de recherche à la mise en oeuvre de l'amélioration du positionnement de leur site web ou application web."
+        />
       </div>
     </div>
   );
