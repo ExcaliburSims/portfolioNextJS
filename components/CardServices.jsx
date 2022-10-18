68831d9b71ec59c6b88c3e3b05d6fbaaf9261b7f
ExcaliburSims <ximeonzilu@gmail.com>
2022-10-18 07:11:03 +0100

    fix

--- components/CardServices.jsx
+++ components/CardServices.jsx
@@ -6,12 +6,7 @@ function CardServices(props) {
     <div className={styles.cardService}>
       <div className={styles.cardIcon}>{props.icon}</div>
       <h3>{props.title}</h3>
-      <p>
-        remaining essentially unchanged. It was popularised in the 1960s with
-        the release of Letraset sheets containing Lorem Ipsum passages, and more
-        recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
-      </p>
+      <p>{props.description}</p>
     </div>
   );
 }
