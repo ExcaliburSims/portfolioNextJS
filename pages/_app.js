69cc3ae6d48b504f6cb030cb450fa93483eb30ec
ExcaliburSims <ximeonzilu@gmail.com>
2022-10-19 11:14:36 +0100

    feat: fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import '../styles/reset.css'
-import Header from '../components/Header'
+import { useEffect, useState } from "react";
+import "../styles/reset.css";
+import LoadingScreen from "./Loading";
 
 function MyApp({ Component, pageProps }) {
-  
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 8000);
+  }, []);
+
+  return <>{!loading ? <Component {...pageProps} /> : <LoadingScreen />}</>;
 }
 
-export default MyApp
+export default MyApp;
